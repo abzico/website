d6f2e55b3a58550b4a95165463448ad543f89fd9
Wasin Thonkaew <haxpor@gmail.com>
2018-01-25 05:31:40 +0800

    Improve workflow

--- scripts/build.js
+++ scripts/build.js
@@ -8,41 +8,41 @@ const replace = require('replace');
 const uglifyjs = require('uglify-es');
 const uglifycss = require('uglifycss');
 
-// ensure that 'bulid' folder is created
-fsextra.emptyDirSync('build');
+// ensure that '_site' folder is created
+fsextra.emptyDirSync('_site');
 
 // copy in all assets and .html
-fsextra.copySync('assets', 'build/assets');
-fsextra.copySync('images', 'build/images');
-fsextra.copySync('index.html', 'build/index.html');
-fsextra.copySync('favicon.ico', 'build/favicon.ico');
+fsextra.copySync('assets', '_site/assets');
+fsextra.copySync('images', '_site/images');
+fsextra.copySync('index.html', '_site/index.html');
+fsextra.copySync('favicon.ico', '_site/favicon.ico');
 
 // uglify and minify main.js
 var result = uglifyjs.minify({
-	'main.js': fs.readFileSync('build/assets/js/main.js', 'utf8')
+	'main.js': fs.readFileSync('_site/assets/js/main.js', 'utf8')
 });
-fs.writeFileSync('build/assets/js/main.min.js', result.code, 'utf8');
+fs.writeFileSync('_site/assets/js/main.min.js', result.code, 'utf8');
 
 // uglify and minify main.css
-result = uglifycss.processFiles(['build/assets/css/main.css']);
-fs.writeFileSync('build/assets/css/main.min.css', result, 'utf8');
+result = uglifycss.processFiles(['_site/assets/css/main.css']);
+fs.writeFileSync('_site/assets/css/main.min.css', result, 'utf8');
 
 // remove input file from bulid folder
-fsextra.removeSync('build/assets/js/main.js');
-fsextra.removeSync('build/assets/css/main.css');
+fsextra.removeSync('_site/assets/js/main.js');
+fsextra.removeSync('_site/assets/css/main.css');
 
 // sed js inclusion in index.html
 replace({
 	regex: '<link rel="stylesheet" href="assets/css/main.css" />',
 	replacement: '<link rel="stylesheet" href="assets/css/main.min.css" />',
-	paths: ['build/index.html'],
+	paths: ['_site/index.html'],
 	recursive: false,
 	silent: true
 });
 replace({
 	regex: '<script src="assets/js/main.js"></script>',
 	replacement: '<script src="assets/js/main.min.js"></script>',
-	paths: ['build/index.html'],
+	paths: ['_site/index.html'],
 	recursive: false,
 	silent: true
 });
